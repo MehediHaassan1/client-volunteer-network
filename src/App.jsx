86697bb15213c18a5d20207912e15e0ebe27e757
Mehedi Hassan Barat <mehedi.haassan1@gmail.com>
2024-01-26 08:42:13 +0600

    Navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
     return (
         <>
+            <NavBar></NavBar>
             <h1 className="text-3xl font-bold underline">Hello world!</h1>
         </>
     );
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import logo from "../../assets/logos/Group 1329.png";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+    const navItems = (
+        <>
+            <li className="mx-2 font-semibold px-1 py-1">
+                <Link to="/">Home</Link>
+            </li>
+            <li className="mx-2 font-semibold px-1 py-1">
+                <Link to="/donation">Donation</Link>
+            </li>
+            <li className="mx-2 font-semibold px-1 py-1">
+                <Link to="/events">Events</Link>
+            </li>
+            <li className="mx-2 font-semibold px-1 py-1">
+                <Link to="/blog">Blog</Link>
+            </li>
+            <li className="mx-2 font-semibold bg-purple-300 px-1 py-1 rounded">
+                <Link to="/register">Register</Link>
+            </li>
+            <li className="mx-2 font-semibold bg-orange-300 px-1 py-1 rounded">
+                <Link to="/admin">Admin</Link>
+            </li>
+        </>
+    );
+    return (
+        <div className="navbar bg-base-100 max-w-7xl mx-auto p-0">
+            <div className="navbar-start">
+                <Link to="/" className="btn btn-ghost text-xl h-20">
+                    <img src={logo} alt="logo" className="h-10" />
+                </Link>
+            </div>
+
+            <div className="navbar-end">
+                <ul className=" hidden lg:flex menu menu-horizontal px-1">
+                    {navItems}
+                </ul>
+
+                <div className="dropdown dropdown-end">
+                    <div
+                        tabIndex={0}
+                        role="button"
+                        className="btn btn-ghost lg:hidden"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-5 w-5"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M4 6h16M4 12h8m-8 6h16"
+                            />
+                        </svg>
+                    </div>
+                    <ul
+                        tabIndex={0}
+                        className="menu menu-sm gap-2 lg:gap-0 dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+                    >
+                        {navItems}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
